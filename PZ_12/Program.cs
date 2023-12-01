d2319767560f82d9f48c4af92dbe36991dc3bb38
Tushev-Dmitriy <tushev-dmitriy@mail.ru>
2023-12-01 15:20:56 +0500

    добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите периметр комнаты");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите высоту комнаты");
+            double H = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Понадобится " + Rulon(P, H) + " рулонов");
+        }
+        static double Rulon(double P, double H)
+        {
+            double rulon = (P * H) / 10;
+            return rulon;
+        }
+    }
+}
