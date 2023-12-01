4b6feedd8776b6935762350d6fd4bacc0c1f32b1
Tushev-Dmitriy <tushev-dmitriy@mail.ru>
2023-12-01 15:18:19 +0500

    добавлена 11 практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void TrianglePS(double a) //Метод подсчета периметра и площади
+        {
+            double P = 3 * a; //подсчет периметра
+            Console.WriteLine("Периметр треугольника равен " + P); //вывод полученного результата
+            double S = Math.Pow(a, 2) * (Math.Sqrt(3) / 4); //подсчет площади
+            Console.WriteLine("Площадь треугольника равна " + S); //вывод полученного результата
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение а");
+            double a = Convert.ToDouble(Console.ReadLine()); //сохранение значения а
+            TrianglePS(a); //вызов метода
+        }
+    }
+}
